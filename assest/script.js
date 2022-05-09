fe4624f0a856ee8ad4861627624032338271d325
KristinaSolo <97984146+KristinaSolo@users.noreply.github.com>
2022-05-09 21:58:12 +0300

    new

---
+++ assest/script.js
@@ -0,0 +1,31 @@
+function select(sender){
+sender.classList.add('selected');
+}
+
+
+
+
+// function sum(a, b){
+//     return a - b;
+// }
+
+
+// let sum2 = function (a, b){
+//     return a + b;
+// };
+
+
+// (function(a, b){
+//     console.log(a * b);
+// })(10, 3);
+
+
+// // let result = sum2 (10, 3);
+// // console.log(result);
+
+
+// let sum3 = (a, b) => a + b;
+// let result = sum3(12, 20);
+
+
+// // console.log(result);
